2cc2dc8e8b40d78959b2519752cf178349e327f5
david-parkk <ji0513ji@naver.com>
2023-10-24 00:09:31 +0900

    init

---
+++ src/main/java/TSM/demo/TsmBackendApplication.java
@@ -0,0 +1,13 @@
+package TSM.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsmBackendApplication.class, args);
+	}
+
+}
