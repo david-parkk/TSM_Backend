1d312f4cdb3cb6115ec8fa5af76757951f974e23
david-parkk <ji0513ji@naver.com>
2023-11-28 00:25:13 +0900

    refactoring unwell matching조회 #25

--- src/main/java/TSM/demo/controller/MatchingController.java
+++ src/main/java/TSM/demo/controller/MatchingController.java
@@ -4,10 +4,11 @@ package TSM.demo.controller;
 import TSM.demo.domain.Matching;
 import TSM.demo.domain.State;
 import TSM.demo.domain.place.*;
-import TSM.demo.repository.query.MatchingDto;
 
 import TSM.demo.repository.query.MatchingResponseDto;
-import TSM.demo.repository.query.SuccessMatchingDto;
+import TSM.demo.repository.query.unwell.UnwellSuccessMatchingDto;
+import TSM.demo.repository.query.unwell.UnwellMatchingColumnDto;
+import TSM.demo.repository.query.unwell.UnwellMatchingDto;
 import TSM.demo.service.CourseService;
 
 import TSM.demo.domain.User;
@@ -19,12 +20,10 @@ import TSM.demo.service.PlaceService;
 import TSM.demo.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -32,7 +31,6 @@ import org.springframework.web.servlet.ModelAndView;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -45,33 +43,21 @@ public class MatchingController {
     private final UserService userService;
 
 
-
-    @GetMapping("/unwell")
-    public ModelAndView unwellMatching(ModelAndView mav, HttpSession httpSession) {
+    public List<MatchingResponseDto> findUnwellMatchingResponseDto(User unwell){
         List<Matching> matchings = new ArrayList<>();
-        List<MatchingResponseDto> matchingResponseDtos = new ArrayList<>();
-        List<SuccessMatchingDto> successMatchingDtos = new ArrayList<>();
-        User finduser = userService.findOneByEmail((String) httpSession.getAttribute("email"));
-
-        if (finduser.getIsVolunteer() == 1) {
-            matchings.addAll(matchingService.findAllByVolunteerId(finduser.getId()));
-        } else {
-            matchings.addAll(matchingService.findAllByUnwellId(finduser.getId()));
-        }
-        System.out.println("matchings.size() = " + matchings.size());
+        matchings.addAll(matchingService.findAllByUnwellId(unwell.getId()));
         List<Course> courses = courseService.findAllCourseByMatching(matchings);
         List<Restaurant> restaurants = placeService.findAllRestaurantByMatching(matchings);
         List<Room> rooms = placeService.findAllRoomByMatching(matchings);
         List<Transport> transports = placeService.findAllTransportByMatching(matchings);
         List<TravelPlace> travelPlaces = placeService.findAllTravelPlaceByMatching(matchings);
+
+        List<MatchingResponseDto>matchingResponseDtos=new ArrayList<>();
         for (Matching matching : matchings) {
-            if (matching.getVolunteerId() == 0 && finduser.getIsVolunteer() == 1)
-                continue;
             if (matching.getRequestType() == 1) {
                 for (Course course : courses) {
                     if (course.getId() == matching.getRequestId()) {
-
-                        matchingResponseDtos.add(new MatchingResponseDto(course.getName(), matching.getRequestType(), matching.getStartTime(), matching.getEndTime(), finduser.getIsVolunteer(), matching.getId(), matching.getGroupId(), matching.getSickId(), matching.getVolunteerId()
+                        matchingResponseDtos.add(new MatchingResponseDto(course.getName(), matching.getRequestType(), matching.getStartTime(), matching.getEndTime(), unwell.getIsVolunteer(), matching.getId(), matching.getGroupId(), matching.getSickId(), matching.getVolunteerId()
                                 , matching.getState(), "Course"));
                         break;
                     }
@@ -79,7 +65,7 @@ public class MatchingController {
             } else if (matching.getRequestType() == 2) {
                 for (Restaurant restaurant : restaurants) {
                     if (restaurant.getId() == matching.getRequestId()) {
-                        matchingResponseDtos.add(new MatchingResponseDto(restaurant.getName(), matching.getRequestType(), matching.getStartTime(), matching.getEndTime(), finduser.getIsVolunteer(), matching.getId(), matching.getGroupId(), matching.getSickId(), matching.getVolunteerId()
+                        matchingResponseDtos.add(new MatchingResponseDto(restaurant.getName(), matching.getRequestType(), matching.getStartTime(), matching.getEndTime(), unwell.getIsVolunteer(), matching.getId(), matching.getGroupId(), matching.getSickId(), matching.getVolunteerId()
                                 , matching.getState(), "Restaurant"));
                         break;
                     }
@@ -87,7 +73,7 @@ public class MatchingController {
             } else if (matching.getRequestType() == 3) {
                 for (Room room : rooms) {
                     if (room.getId() == matching.getRequestId()) {
-                        matchingResponseDtos.add(new MatchingResponseDto(room.getName(), matching.getRequestType(), matching.getStartTime(), matching.getEndTime(), finduser.getIsVolunteer(), matching.getId(), matching.getGroupId(), matching.getSickId(), matching.getVolunteerId()
+                        matchingResponseDtos.add(new MatchingResponseDto(room.getName(), matching.getRequestType(), matching.getStartTime(), matching.getEndTime(), unwell.getIsVolunteer(), matching.getId(), matching.getGroupId(), matching.getSickId(), matching.getVolunteerId()
                                 , matching.getState(), "Room"));
                         break;
                     }
@@ -95,7 +81,7 @@ public class MatchingController {
             } else if (matching.getRequestType() == 4) {
                 for (Transport transport : transports) {
                     if (transport.getId() == matching.getRequestId()) {
-                        matchingResponseDtos.add(new MatchingResponseDto(transport.getName(), matching.getRequestType(), matching.getStartTime(), matching.getEndTime(), finduser.getIsVolunteer(), matching.getId(), matching.getGroupId(), matching.getSickId(), matching.getVolunteerId()
+                        matchingResponseDtos.add(new MatchingResponseDto(transport.getName(), matching.getRequestType(), matching.getStartTime(), matching.getEndTime(), unwell.getIsVolunteer(), matching.getId(), matching.getGroupId(), matching.getSickId(), matching.getVolunteerId()
                                 , matching.getState(), "Transport"));
                         break;
                     }
@@ -103,8 +89,7 @@ public class MatchingController {
             } else if (matching.getRequestType() == 5) {
                 for (TravelPlace travelPlace : travelPlaces) {
                     if (travelPlace.getId() == matching.getRequestId()) {
-
-                        matchingResponseDtos.add(new MatchingResponseDto(travelPlace.getName(), matching.getRequestType(), matching.getStartTime(), matching.getEndTime(), finduser.getIsVolunteer(), matching.getId(), matching.getGroupId(), matching.getSickId(), matching.getVolunteerId()
+                        matchingResponseDtos.add(new MatchingResponseDto(travelPlace.getName(), matching.getRequestType(), matching.getStartTime(), matching.getEndTime(), unwell.getIsVolunteer(), matching.getId(), matching.getGroupId(), matching.getSickId(), matching.getVolunteerId()
                                 , matching.getState(), "Travel_Place"));
                         break;
                     }
@@ -116,32 +101,74 @@ public class MatchingController {
                 continue;
             User findUser = userService.findOne(matchingResponseDto.getVolunteerId());
             matchingResponseDto.setVolunteerName(findUser.getName());
+        }
+        return matchingResponseDtos;
+    }
+
+    public List<MatchingResponseDto> addMemberInfoUnwellMatchingResponseDto(List<MatchingResponseDto>matchingResponseDtos,User unwell){
+        for (MatchingResponseDto matchingResponseDto : matchingResponseDtos) {
+            User volunteer = userService.findOne(matchingResponseDto.getVolunteerId());
+            if (volunteer == null)
+                continue;
+            matchingResponseDto.setVolunteerInfo(volunteer.getName(), volunteer.getEmail(), volunteer.getPhoneNum());
+            matchingResponseDto.setSickName(unwell.getName());
+        }
+        return matchingResponseDtos;
+    }
+
+    public List<UnwellSuccessMatchingDto> findUnwellSuccessMatchingDto(List<MatchingResponseDto> matchingResponseDtos,User unwell){
+        List<UnwellSuccessMatchingDto> unwellSuccessMatchingDtos=new ArrayList<>();
+        for (MatchingResponseDto matchingResponseDto : matchingResponseDtos) {
+            if (matchingResponseDto.getState() == State.SUCCESS) {
+                unwellSuccessMatchingDtos.add(new UnwellSuccessMatchingDto(matchingResponseDto.getName(), matchingResponseDto.getRequestString(),
+                        matchingResponseDto.getStartTime(), matchingResponseDto.getEndTime(), matchingResponseDto.getVolunteerName(), matchingResponseDto.getVolunteerName(), matchingResponseDto.getVolunteerPhoneNum(), matchingResponseDto.getGroupId()));
+            }
 
         }
-        System.out.println("matchingResponseDtos.size() = " + matchingResponseDtos.size());
-        for (MatchingResponseDto matchingResponseDto : matchingResponseDtos) {
-            //System.out.println("matchingResponseDto.getRequestString() = " + matchingResponseDto.getRequestString());
-            if (finduser.getIsVolunteer() == 0) {
-                User user = userService.findOne(matchingResponseDto.getVolunteerId());
-                if (user == null)
-                    continue;
-                matchingResponseDto.setVolunteerInfo(user.getName(), user.getEmail(), user.getPhoneNum());
-                matchingResponseDto.setSickName(finduser.getName());
-                if (matchingResponseDto.getState() == State.SUCCESS) {
-                    successMatchingDtos.add(new SuccessMatchingDto(matchingResponseDto.getName(), matchingResponseDto.getRequestString(),
-                            matchingResponseDto.getStartTime(), matchingResponseDto.getEndTime(), user.getName(), user.getEmail(), user.getPhoneNum(), matchingResponseDto.getGroupId()));
-                }
+        return unwellSuccessMatchingDtos;
+    }
 
-            } else {
-                User user = userService.findOne(matchingResponseDto.getSickId());
-                matchingResponseDto.setUnwellInfo(user.getName(), user.getEmail(), user.getPhoneNum());
-                matchingResponseDto.setVolunteerName(finduser.getName());
+    @GetMapping("/unwell")
+    public ModelAndView unwellMatching(ModelAndView mav, HttpSession httpSession) {
+
+        List<UnwellMatchingDto>unwellMatchingDtos=new ArrayList<>();
+        List<UnwellSuccessMatchingDto> successMatchingDtos = new ArrayList<>();
+        //본인 유저정보 찾기
+        User finduser = userService.findOneByEmail((String) httpSession.getAttribute("email"));
+        //본인과 관련된 matching 정보 받기
+        List<MatchingResponseDto> matchingResponseDtos=findUnwellMatchingResponseDto(finduser);
+        //matchingResponseDto에 unwell volunteer정보 초기화
+        List<MatchingResponseDto> newMatchingResponseDtos=addMemberInfoUnwellMatchingResponseDto(matchingResponseDtos,finduser);
+
+        boolean isInsert;
+        //받아온 matchingResponseDto를 가지고 UnwellMatchingDto생성
+        for (MatchingResponseDto matching: newMatchingResponseDtos){
+            isInsert=false;
+            //이미 처리된 matching 정보는 건너뜀
+            if(matching.getState()==State.SUCCESS||matching.getState()== State.FAIL)
+                continue;
+            for(UnwellMatchingDto unwellMatchingDto: unwellMatchingDtos){
+                if(unwellMatchingDto.getGroupId()==matching.getGroupId()){
+                    unwellMatchingDto.getUnwellMatchingColumnDtoList().add(new UnwellMatchingColumnDto(matching.getVolunteerName(),matching.getVolunteerId(),matching.getState(),matching.getId(),matching.getGroupId()));
+                    isInsert=true;
+                    break;
+                }
+            }
+            if(!isInsert){
+                UnwellMatchingDto unwellMatchingDto = new UnwellMatchingDto(matching.getName(), matching.getRequestType(), matching.getRequestString(), matching.getStartTime(), matching.getEndTime(), matching.getGroupId());
+                unwellMatchingDtos.add(unwellMatchingDto);
+                if(matching.getVolunteerId()!=0){
+                    unwellMatchingDto.getUnwellMatchingColumnDtoList().add(new UnwellMatchingColumnDto(matching.getVolunteerName(),matching.getVolunteerId(),matching.getState(),matching.getId(),matching.getGroupId()));
+                }
             }
         }
-        if (finduser.getIsVolunteer() == 0)
-            mav.setViewName("unwell_matching");
-        else
-            mav.setViewName("volunteer_matching_result");
+        //성공한 매칭 따로 가져오기
+        successMatchingDtos=findUnwellSuccessMatchingDto(newMatchingResponseDtos,finduser);
+
+        mav.addObject("matchings", unwellMatchingDtos);
+        mav.addObject("successMatchings", successMatchingDtos);
+        mav.setViewName("unwell_matching");
+
         return mav;
     }
     @GetMapping("/volunteer")
@@ -243,7 +270,7 @@ public class MatchingController {
     public ModelAndView volunteerMatching(ModelAndView mav, HttpSession httpSession) {
         List<Matching> matchings = new ArrayList<>();
         List<MatchingResponseDto> matchingResponseDtos = new ArrayList<>();
-        List<SuccessMatchingDto> successMatchingDtos = new ArrayList<>();
+        List<UnwellSuccessMatchingDto> successMatchingDtos = new ArrayList<>();
         User finduser = userService.findOneByEmail((String) httpSession.getAttribute("email"));
 
         if (finduser.getIsVolunteer() == 1) {
@@ -321,7 +348,7 @@ public class MatchingController {
                 matchingResponseDto.setVolunteerInfo(user.getName(), user.getEmail(), user.getPhoneNum());
                 matchingResponseDto.setSickName(finduser.getName());
                 if (matchingResponseDto.getState() == State.SUCCESS) {
-                    successMatchingDtos.add(new SuccessMatchingDto(matchingResponseDto.getName(), matchingResponseDto.getRequestString(),
+                    successMatchingDtos.add(new UnwellSuccessMatchingDto(matchingResponseDto.getName(), matchingResponseDto.getRequestString(),
                             matchingResponseDto.getStartTime(), matchingResponseDto.getEndTime(), user.getName(), user.getEmail(), user.getPhoneNum(), matchingResponseDto.getGroupId()));
                 }
 
